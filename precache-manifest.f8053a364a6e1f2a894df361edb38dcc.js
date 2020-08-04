ef8f90aa0d028d18ab9e59dc144a4bff4996445c
Dawid Piwko <dpiwko@gmail.com>
2020-08-04 20:38:22 +0200

    Updates

---
+++ precache-manifest.f8053a364a6e1f2a894df361edb38dcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf695116c03835d61be193fa0bb65969",
+    "url": "/merchants-list/index.html"
+  },
+  {
+    "revision": "dffe407d45b5671c4697",
+    "url": "/merchants-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4f8c28d2a4249f3b4ebf",
+    "url": "/merchants-list/static/js/2.873d9ed3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/merchants-list/static/js/2.873d9ed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffe407d45b5671c4697",
+    "url": "/merchants-list/static/js/main.375faf4a.chunk.js"
+  },
+  {
+    "revision": "9b439c09dbee265564eb",
+    "url": "/merchants-list/static/js/runtime-main.dee0da38.js"
+  }
+]);
--- src/components/Container/Container.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-import { containerWidth, unit } from 'assets/styles'
-
-export default styled('div')({
-  width: containerWidth,
-  padding: unit * 2,
-  margin: '0 auto',
-})
